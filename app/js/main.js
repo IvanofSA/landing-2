6d8658a2d269b57494506baef2deab6e936d275a
IvanofSA <poshelya@gmail.com>
2016-07-04 22:54:04 +0300

    mobile

--- app/js/main.js
+++ app/js/main.js
@@ -97,6 +97,29 @@ $(document).ready(function () {
             }
         }
     });
+
+    $(".toggle_mnu").click(function() {
+        $(".sandwich").toggleClass("active");
+    });
+
+    $(".top_mnu ul a").click(function() {
+        $(".top_mnu").fadeOut(600);
+        $(".sandwich").toggleClass("active");
+        $(".top_text").css("opacity", "1");
+    }).append("<span>");
+
+    $(".toggle_mnu").click(function() {
+        if ($(".top_mnu").is(":visible")) {
+            $(".top_text").css("opacity", "1");
+            $(".top_mnu").fadeOut(600);
+            $(".top_mnu li a").removeClass("fadeInUp animated");
+        } else {
+            $(".top_text").css("opacity", ".1");
+            $(".top_mnu").fadeIn(600);
+            $(".top_mnu li a").addClass("fadeInUp animated");
+        };
+    });
+
 });//jQue
 
 
@@ -134,5 +157,4 @@ $(window).scroll(function () {
                 $(this).addClass('transbg');
             }
         });
-
 });
